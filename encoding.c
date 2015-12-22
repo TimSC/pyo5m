b2122ab500a74a247db4db72eedb0732e3ef2a34
Tim Sheerman-Chase <orders2008@sheerman-chase.org.uk>
2015-12-22 19:35:50 +0000

    Add zigzag encoding

--- encoding.c
+++ encoding.c
@@ -1,28 +1,59 @@
 #include <Python.h>
 
-#define INTERNAL_BUFF_SIZE 128
+#define INTERNAL_BUFF_SIZE 16
 
 static PyObject *
 EncodeVarint(PyObject *self, PyObject *args)
 {
     unsigned PY_LONG_LONG val = 0;
 	char buff[INTERNAL_BUFF_SIZE] = "";
+	unsigned char more = 1;
+	unsigned int cursor = 0;
 
     if (!PyArg_ParseTuple(args, "K", &val))
         return NULL;
 	
-	unsigned char more = 1;
-	unsigned int cursor = 0;
 	while (more) {
 		unsigned char sevenBits = val & 0x7f;
 		val = val >> 7;
-		more = val > 0;
+		more = val != 0;
 		if(cursor < INTERNAL_BUFF_SIZE) {
 			buff[cursor] = (more << 7) + sevenBits;
 			cursor ++;	
 		}
 		else {
-			PyErr_SetString(PyExc_TypeError, "Internal buffer overflow while encoding varint");
+			PyErr_SetString(PyExc_RuntimeError, "Internal buffer overflow while encoding varint");
+			return NULL;
+		}
+	}
+
+    return PyString_FromStringAndSize(buff, cursor);
+}
+
+static PyObject *
+EncodeZigzag(PyObject *self, PyObject *args)
+{
+    PY_LONG_LONG val = 0;
+	char buff[INTERNAL_BUFF_SIZE] = "";
+	unsigned char more = 1;
+	unsigned int cursor = 0;
+	unsigned PY_LONG_LONG zz = 0;
+
+    if (!PyArg_ParseTuple(args, "L", &val))
+        return NULL;
+
+	zz = (val << 1) ^ (val >> (sizeof(PY_LONG_LONG)*8-1));
+
+	while (more) {
+		unsigned char sevenBits = zz & 0x7f;
+		zz = zz >> 7;
+		more = zz != 0;
+		if(cursor < INTERNAL_BUFF_SIZE) {
+			buff[cursor] = (more << 7) + sevenBits;
+			cursor ++;	
+		}
+		else {
+			PyErr_SetString(PyExc_RuntimeError, "Internal buffer overflow while encoding zigzag");
 			return NULL;
 		}
 	}
@@ -31,9 +62,9 @@ EncodeVarint(PyObject *self, PyObject *args)
 }
 
 static PyMethodDef SpamMethods[] = {
-    {"EncodeVarint",  EncodeVarint, METH_VARARGS,
-     "Encode a varint."},
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+    {"EncodeVarint",  EncodeVarint, METH_VARARGS, "Varint encode an unsigned number."},
+	{"EncodeZigzag",  EncodeZigzag, METH_VARARGS, "Zigzag encode a signed number."},
+    {NULL, NULL, 0, NULL}
 };
 
 PyMODINIT_FUNC
